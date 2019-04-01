2c9e72cfb1ffea506ea7591713eb65dee587b759
Geoff <ethos42@gmail.com>
2019-04-01 14:56:28 +0000

    test

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -23,6 +23,7 @@ namespace web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
             }
 
             app.Run(async (context) =>
